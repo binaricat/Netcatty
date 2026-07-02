1332db0e0e5682c74f48abf2bdc6f061acb31d0b
bincxz <16399091+binaricat@users.noreply.github.com>
2026-07-02 15:54:20 +0800

    Update Nix release metadata for v1.1.55

--- nix/release.nix
+++ nix/release.nix
@@ -1,14 +1,14 @@
 {
-  version = "1.1.54";
+  version = "1.1.55";
 
   sources = {
     x86_64-linux = {
       appImageArch = "x86_64";
-      hash = "sha256-QohX1ntkB3t36NH9nu0FIOuks1fzFm1CGGZim3M8Kfc=";
+      hash = "sha256-90ii0mMasW3yeMGy54OzRewEayUftktEBg6FPSjDtcE=";
     };
     aarch64-linux = {
       appImageArch = "arm64";
-      hash = "sha256-Jvp0MGs7C+Vx+/zE+oWPC2JOIznzx7qWguQ05CuL32s=";
+      hash = "sha256-HJ/BtJs7AVLSktTvcsN0DgKyBDVIJ/0K6a0go/IUdRc=";
     };
   };
 }
